bbec37b3315e3b2d79e581b35ae319c5e5b8db3b
xzchen <xzchen65@126.com>
2016-04-04 16:17:29 +0800

    以后要记得回来看看
    其实这个本来没什么，只是因为task002_17写的久，自己看着问题产生，然后又尽量修改了得，当然还是留了很多问题了。
    想着以后再看的时候，这么简单的code，结果还是写了那么多注释，以前得是有多蠢啊，希望那时候我还是在写，并且已经写得好些了。

--- task002/task002_16/main.js
+++ task002/task002_16/main.js
@@ -3,41 +3,78 @@ window.onload = function() {
 	// 获取添加的button元素
 	var aBtn = table.getElementsByTagName("button")
 	
-
 	//在点击添加按钮的时候判断是否符合正则要求，成则渲染表格。否则显示提示
 	function render() {
-		//获取操作元素
-		var cityName = document.getElementById("aqi-city-input").value
-		var value = document.getElementById("aqi-value-input").value
+		//获取数据和操作元素
+		var cityName = document.getElementById("aqi-city-input").value.trim()
+		// 这里要用到trim()方法去掉字符串两边空格，否则影响判断结果
+		var value = document.getElementById("aqi-value-input").value.trim()
 		var citySpan = document.getElementById("aqi-city-span")
 		var valueSpan = document.getElementById("aqi-value-span")
 
-		//正则表达式,匹配规则
-		var re_cityName = /[\u4e00-\u9fa5]/g;
-		var re_value = /\d/g;
+		//正则表达式,匹配规则,这里还是特懵，边用边看吧
+		var re_cityName = /^[A-Za-z\u4e00-\u9fa5]+$/;
+		var re_value = /^\d{2,3}$/;
 		
-		
-		// 判断语句
+		//清空提示的内容
+		citySpan.innerHTML = "";
+		valueSpan.innerHTML = "";
+
+		var reCity=false;
+		// 判断语句，看是否符合输入规则，这是城市名和污染值都符合要求
 		if ( re_cityName.test(cityName) && re_value.test(value)) {
-			var html = "";
-			html = "<tr><td>" + cityName + "</td><td>" +value +"</td><td><button>删除</button></tr>"
-			table.innerHTML += html;
+			// 这里为了防止城市输入重复，写了比较复杂的判断。挺麻烦的，但是别人的自己又不太理解
+
+			// 加入第一行数据，不进行判断是否重复
+			if (aBtn.length == 0) {
+				var html = "";
+				html = "<tr><td>" + cityName + "</td><td>" +value +"</td><td><button data-cityName = "+cityName+">删除</button></tr>"
+				// 用childern[1]获取tbody元素。不加这个的话，每次文档中会自动补入一个新的tbody
+				table.children[1].innerHTML += html;
+			}
+
+			// 当表格已经渲染之后，判断是否重复
+			else {
+				//按照已经渲染的，选取每行下的button，通过节点切换？知晓城市是否重复，是则将重新输入的空气污染质量值替换之前的
+				for (var i = 0; i < aBtn.length; i++) {
+					if (aBtn[i].parentNode.parentNode.firstChild.innerHTML == cityName) {
+						aBtn[i].parentNode.parentNode.children[1].innerHTML = value;	
+						reCity = true;
+						break;
+					}
+				}
+
+				//City如果此刻还等于false，说明没有重复的从城市，那直接渲染就好了。
+				if (reCity == false) {
+				html = "<tr><td>" + cityName + "</td><td>" +value +"</td><td><button data-cityName = "+cityName+">删除</button></tr>"
+				// 用childern[1]获取tbody元素。不加这个的话，每次文档中会自动补入一个新的tbody
+				table.children[1].innerHTML += html;
+				}		
+			}
 		}
-		
-		else if (!re_cityName.test(cityName)) {
-			// 为何两次打印的结果不一样
-			console.log(!re_cityName.test(cityName))
-			console.log(!re_cityName.test(cityName))
-			citySpan.innerHTML = "请输入中文"
+
+		//城市名和污染值都不符合输入要求
+		else if (!re_cityName.test(cityName) && (!re_value.test(value))) {
+			// 为何两次打印的结果不一样，这里在用到了全局搜索的时候，记得注意lastIndex的问题。
+			citySpan.innerHTML = "请输入中文或者英文字符，不可包含数字"
+			valueSpan.innerHTML = "请输入2-3位的整数"
+		} 
+
+		else if (!re_cityName.test(cityName) && (re_value).test(value)) {	
+			citySpan.innerHTML = "请输入中文或者英文字符"
 		}
 
 		else  {
-			valueSpan.innerHTML = "请输入数字"
+			valueSpan.innerHTML = "请输入2-3位的整数"
 		}
 
+		// 给删除的button统一添加点击事件，删除加入的数据
+		for (var i =0; i < aBtn.length; i++) {
+			aBtn[i].onclick = function() {
+				this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode)
+			}
+		}
 	}
-
-	
 
 	// 给按钮添加事件，并向其中渲染出来的button添加了click事件
 	function btnHandle() {
@@ -55,4 +92,3 @@ window.onload = function() {
 
 	init();
 }
-
---
+++ task002/task002_17/main.js
@@ -0,0 +1,167 @@
+window.onload = function() {
+	// 获取操作元素
+	var fieldset=document.getElementById("form-gra-time")
+	var aSpan = fieldset.getElementsByTagName("span")
+	var oSelect = document.getElementById("city-select");
+	var aqiChartWrap = document.getElementById("aqi-chart-wrap")
+	var aDiv = aqiChartWrap.getElementsByTagName("div")
+	
+	//定义随机颜色
+	var colors = ["#146464", "#17a5a3", "#11cc7c", "#58e4e3", "#a4c1c1", "#8c9696", "#1386d4", "#0b56c1", "#94a727"]
+
+	// 切换选项,这里本来是用display:block/none来控制div的显示和隐藏的，但是会影响到display:flex的布局，就改成了修改透明度。不控制
+	for (var i = 0; i < aSpan.length; i++) {
+		aSpan[i].index = i;
+		aSpan[i].onclick = function() {
+			for (var i = 0; i < aSpan.length; i++) {
+				aSpan[i].className = ""
+				//还是改成了用位置改变。这里不能仅仅只用opacity,那么当移动当其他元素，如月份柱子上，即使看不见月份柱子，title还是会出现，同样也不能单独用Z-index，否则点击span的时候，title是正确的，但是看见的只有每日柱子
+				aDiv[i].style.top = "600px"
+			}
+			this.className = "active"
+			aDiv[this.index].style.top = "0"
+		}
+	}
+
+	getTime()	
+	// 初始渲染页面
+	function getTime() {
+		// 定义时间初始值这里用引号括起来，不然因为当前时间的月份是从0开始计数的，会影响计算结果
+		var beginTime = new Date("2016, 1, 1")
+		var endTime = new Date()
+		var disTime = parseInt((endTime.getTime() - beginTime.getTime())/1000)
+
+		//或者用Math.floor()会更好？得到有多少天
+		var day = parseInt(disTime/3600/24)
+		
+		// 初始的时候输入北京的数值
+		render(day,500)	
+
+		// 当城市改变的时候，改变Aqi的峰值并渲染
+		oSelect.onchange = function() {	
+			switch (oSelect.selectedIndex) {
+				case 0:
+				case 5:
+				case 8:
+					render(day, 500);
+					break;
+				case 1:
+				case 4:
+					render(day, 300)
+					break;	
+				case 2:
+					render(day, 200)
+					break;
+				default:
+					render(day, 100)
+					break;	
+			}
+
+		}
+	}
+
+	// 渲染空气质量
+	function render(day ,aqi) {
+		// 获取操作元素
+		var oDay = document.getElementById("days")
+		var oWeek = document.getElementById("weeks")
+		var oMonth = document.getElementById("months")
+
+		// 每次切换城市的时候，清空span内容，否则生成的柱子会叠加
+		oDay.innerHTML = ""
+		oWeek.innerHTML = ""
+		oMonth.innerHTML = ""
+
+		//timeTitle定义日期,好进行渲染 compare是和timeTitle进行比较，看月份是否变化
+		var timeTitle = new Date("2015, 12, 31")
+		var compare = new Date("2016, 1, 1")
+
+		//AQI值累加和
+		Total = 0
+
+		//创建数组，存储周/月柱子高度（AQI值）
+		var weekHeight =new Array()
+		var weekTotal = new Array()
+		var monthHeight =new Array()
+		var monthTotal =new Array()
+
+
+		//这个时间来进行第一周（不满7天的）和每个月柱子高度处理的日期判断条件
+		var firstSunday= new Date("2016, 1, 3")
+		var Jan = new Date ("2016, 1, 31")
+		var Feb = new Date ("2016, 2, 29")
+		var Mar = new Date ("2016, 3, 31")
+
+		// 渲染每日空气质量
+		for (var i = 0; i < day; i++) {
+			var oP = document.createElement("p")
+			// 获得随机的高度值，并将高度值量化为AQI值显示在title里
+			var height = Math.round(Math.random() * aqi)
+			oP.style.width = (oDay.offsetWidth)/day/2 + "px"
+			oP.style.height = height + "px"
+			oP.style.backgroundColor = colors[Math.round(Math.random() * 8)]
+
+			// 给每个柱子加title，title的值由timeTitle的各个方法获得
+			timeTitle.setDate(timeTitle.getDate() + 1)
+			compare.setDate(compare.getDate() + 1)
+			oP.title = timeTitle.getFullYear() + "-" + (timeTitle.getMonth()+1) + "-"  +timeTitle.getDate() + "\n" +"[AQI]:" + height
+  			oDay.appendChild(oP)
+  			
+  			//讲每日柱子的高度值累加求和方便进行每周每月的换算
+  			Total += height
+  			
+  			//判断有几周，将每周平均AQI值存储为数组
+      		if (timeTitle.getDay() == 0) {	
+      			// 日期不可以直接比较，差值比下好了	
+      			if (timeTitle - firstSunday == 0) {
+      				height = Math.round((Total / 2)) 
+      			
+      			}
+      			else {
+      				// 这次的累加和减去上次的累加和除以天数可以得到平均值
+      				height = Math.round((Total - weekTotal[weekTotal.length - 1]) / 7)    			
+      			}
+      			weekHeight.push(height) 
+      			weekTotal.push(Total)
+      		}
+
+			
+  			//判断有几月,将每月平均AQI值存储为数组
+  			if (timeTitle.getMonth() != compare.getMonth()) {
+  				if (timeTitle - Jan == 0) {
+  					height = Math.round(Total / 31)
+  				}
+  				else if (timeTitle - Feb == 0) {
+  					height = Math.round((Total - monthTotal[monthTotal.length - 1]) / 29)
+  				}
+  		
+  				else {
+  					height = Math.round((Total - monthTotal[monthTotal.length - 1]) / 31)
+  				}
+  				monthHeight.push(height)
+  				monthTotal.push(Total)
+  			}
+			
+		}
+		
+		// 渲染每周空气质量
+		for (var i =0; i <weekHeight.length; i++) {
+			var oP = document.createElement("p")
+			oP.style.width = (oDay.offsetWidth)/14/2 + "px"
+			oP.style.height = weekHeight[i] + "px"
+			oP.style.backgroundColor = colors[Math.round(Math.random() * 8)]
+			oP.title = "第" + (i+1) + "周的平均" + "\n[AQI]：" + weekHeight[i] 
+			oWeek.appendChild(oP)
+		}
+
+		//渲染每月空气质量
+		for (var i = 0; i < monthHeight.length; i++) {
+			var oP = document.createElement("p")
+			oP.style.width = (oDay.offsetWidth)/3/2 + "px"
+			oP.style.height = monthHeight[i] + "px"
+			oP.style.backgroundColor = colors[Math.round(Math.random() * 8)]
+			oP.title = "第" + (i+1) + "月的平均\n[AQI]：" + monthHeight[i]
+			oMonth.appendChild(oP) 		
+		}	
+	}
+}
